9e48520ef4fc899b5e99c2ebb5d36ae092d9f28a
Suryaprakash <154657173+suryaprakash-it@users.noreply.github.com>
2026-03-29 01:25:24 +0530

    Image

--- FileStream/config.py
+++ FileStream/config.py
@@ -16,9 +16,9 @@ class Telegram:
     FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', False)
     FORCE_SUB = True if str(FORCE_SUB).lower() == "true" else False
     SLEEP_THRESHOLD = int(env.get("SLEEP_THRESHOLD", "60"))
-    FILE_PIC = env.get('FILE_PIC', "https://graph.org/file/5bb9935be0229adf98b73.jpg")
-    START_PIC = env.get('START_PIC', "https://graph.org/file/290af25276fa34fa8f0aa.jpg")
-    VERIFY_PIC = env.get('VERIFY_PIC', "https://graph.org/file/736e21cc0efa4d8c2a0e4.jpg")
+    FILE_PIC = env.get('FILE_PIC', "https://files.catbox.moe/zyoq19.jpg")
+    START_PIC = env.get('START_PIC', "https://files.catbox.moe/zyoq19.jpg")
+    VERIFY_PIC = env.get('VERIFY_PIC', "https://files.catbox.moe/zyoq19.jpg")
     MULTI_CLIENT = False
     FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", None))   # Logs channel for file logs
     ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", None))   # Logs channel for user logs
